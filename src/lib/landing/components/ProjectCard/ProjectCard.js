024a3239d75d6fb4f269e018634f78382dc3828c
GilHeller <gil151200@gmail.com>
2024-10-25 18:55:06 +0300

    UPDATE: responsive

--- src/lib/landing/components/ProjectCard/ProjectCard.js
+++ src/lib/landing/components/ProjectCard/ProjectCard.js
@@ -19,18 +19,18 @@ import { ExpandedList } from './ExpandedList';
 
 const ProjectCard = ({ image_url, title, description, github_url, website_url, language }) => {
   return (
-    <Card sx={{ direction: 'rtl' }}>
+    <Card sx={{ direction: 'rtl' }} >
       <CardMedia
         sx={{ borderRadius: '8px', height: 225, objectFit: 'cover' }}
         image={image_url}
         title={`${title} image`}
       />
       <CardContent>
-        <Stack direction={'row'} justifyContent={'space-between'}>
-          <Typography gutterBottom variant="h4" component="div" color={'primary'} fontWeight={900}>
+        <Box display='flex' flexDirection='row' justifyContent='space-between' flexWrap='wrap'>
+          <Typography gutterBottom variant="h4" component="div" color={'primary'} fontWeight={900} width={'65%'} >
             {title}
           </Typography>
-          <Box>
+          <Box minWidth={88} display='flex' justifyContent='center' alignItems='start' sx={{direction: 'ltr'}}>
             {github_url && (
               <Tooltip title="Check out the project on GitHub">
                 <IconButton href={github_url} target="_blank" rel="noreferrer">
@@ -46,8 +46,10 @@ const ProjectCard = ({ image_url, title, description, github_url, website_url, l
               </Tooltip>
             )}
           </Box>
-        </Stack>
-        <Typography variant="body1">{description}</Typography>
+        </Box>
+        <Typography variant="body1" height={48} alignContent='center'>
+            {description}
+        </Typography>
       </CardContent>
       <CardActions disableSpacing sx={{ gap: '5px', flexWrap: "wrap" }}>
         <ExpandedList
