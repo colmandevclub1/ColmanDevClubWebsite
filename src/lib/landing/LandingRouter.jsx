21a7217ce0de11aa15cbb9f5ee9543a9e147a5d3
GilHeller <gil151200@gmail.com>
2024-10-28 20:48:14 +0200

    UPDATE: imports to absolute in LandingRouter.js

--- src/lib/landing/LandingRouter.jsx
+++ src/lib/landing/LandingRouter.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from 'src/ui/Navbar/Navbar';
-import HomePage from './pages/HomePage';
-import TeamPage from './pages/TeamPage';
-import SyllabusPage from './pages/SyllabusPage/SyllabusPage';
-import { Footer } from './components';
-import { SignInPage, SignUpPage } from '../auth/pages';
+import HomePage from 'src/lib/landing/pages/HomePage';
+import TeamPage from 'src/lib/landing/pages/TeamPage';
+import SyllabusPage from 'src/lib/landing/pages/SyllabusPage/SyllabusPage';
+import { SignInPage, SignUpPage } from 'src/lib/auth/pages';
+import { Footer } from 'src/lib/landing/components';
 import { AuthContextProvider } from '../auth/authContext';
 import ProtectedRoute from 'src/utils/protectedRoutes';
 
